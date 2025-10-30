2a276e1455886a3d90e07d6e762bd9226ac90273
Osiris Aguilazochom <osiris.sagi@gmail.com>
2025-10-30 12:50:01 -0600

    Se modificó la vista de inicio de sesión

--- resources/views/layout/_auth.blade.php
+++ resources/views/layout/_auth.blade.php
@@ -24,11 +24,11 @@
                 <div class="d-flex flex-center flex-wrap px-5">
                     <!--begin::Links-->
                     <div class="d-flex fw-semibold text-primary fs-base">
-                        <a href="#" class="px-5" target="_blank">Terms</a>
+                        <a href="https://secoem.michoacan.gob.mx/aviso-de-privacidad/" class="px-5" target="_blank">Aviso de Privacidad</a>
 
-                        <a href="#" class="px-5" target="_blank">Plans</a>
+                        <a href="https://secoem.michoacan.gob.mx/" class="px-5" target="_blank">Secoem</a>
 
-                        <a href="#" class="px-5" target="_blank">Contact Us</a>
+                        <a href="https://secoem.michoacan.gob.mx/#contacto" class="px-5" target="_blank">Contacto</a>
                     </div>
                     <!--end::Links-->
                 </div>
@@ -37,33 +37,33 @@
             <!--end::Body-->
 
             <!--begin::Aside-->
-            <div class="d-flex flex-lg-row-fluid w-lg-50 bgi-size-cover bgi-position-center order-1 order-lg-2" style="background-image: url({{ image('misc/auth-bg.png') }})">
+            <div class="d-flex flex-lg-row-fluid w-lg-50 bgi-size-cover bgi-position-center order-1 order-lg-2" style="background-image: url({{ image('auth/bg15.png') }})">
                 <!--begin::Content-->
                 <div class="d-flex flex-column flex-center py-7 py-lg-15 px-5 px-md-15 w-100">
                     <!--begin::Logo-->
                     <a href="{{ route('dashboard') }}" class="mb-12">
-                        <img alt="Logo" src="{{ image('logos/custom-1.png') }}" class="h-60px h-lg-75px"/>
+                        <img alt="Logo" src="{{ image('auth/secoem_blanco.svg') }}" class="h-60px h-lg-75px"/>
                     </a>
                     <!--end::Logo-->
 
                     <!--begin::Image-->
-                    <img class="d-none d-lg-block mx-auto w-275px w-md-50 w-xl-500px mb-10 mb-lg-20" src="{{ image('misc/auth-screens.png') }}" alt=""/>
+                    <img class="d-none d-lg-block mx-auto w-275px w-md-50 w-xl-300px mb-10 mb-lg-20" src="{{ image('illustrations/sigma-1/9.png') }}" alt=""/>
                     <!--end::Image-->
 
                     <!--begin::Title-->
                     <h1 class="d-none d-lg-block text-white fs-2qx fw-bolder text-center mb-7">
-                        Fast, Efficient and Productive
+                        Sistema de Control de Denuncias - SICODEN
                     </h1>
                     <!--end::Title-->
 
                     <!--begin::Text-->
                     <div class="d-none d-lg-block text-white fs-base text-center">
-                        In this kind of post, <a href="#" class="opacity-75-hover text-warning fw-bold me-1">the blogger</a>
+                        La herramienta oficial de la Secretaría para la <a href="#" class="opacity-75-hover text-warning fw-bold me-1">recepción, control y seguimiento</a>
 
-                        introduces a person they’ve interviewed <br/> and provides some background information about
+                        de los procedimientos de denuncia. <br/> Un sistema diseñado para optimizar los flujos de trabajo de su área con
 
-                        <a href="#" class="opacity-75-hover text-warning fw-bold me-1">the interviewee</a>
-                        and their <br/> work following this is a transcript of the interview.
+                        <a href="#" class="opacity-75-hover text-warning fw-bold me-1">precisión y rapidez.</a>
+                        
                     </div>
                     <!--end::Text-->
                 </div>
--- resources/views/pages/auth/login.blade.php
+++ resources/views/pages/auth/login.blade.php
@@ -7,13 +7,13 @@
         <div class="text-center mb-11">
             <!--begin::Title-->
             <h1 class="text-gray-900 fw-bolder mb-3">
-                Sign In
+                Bienvenido
             </h1>
             <!--end::Title-->
 
             <!--begin::Subtitle-->
             <div class="text-gray-500 fw-semibold fs-6">
-                Your Social Campaigns
+                SICODEN - Sistema de Control de Denuncias
             </div>
             <!--end::Subtitle--->
         </div>
@@ -22,7 +22,7 @@
         <!--begin::Login options-->
         <div class="row g-3 mb-9">
             <!--begin::Col-->
-            <div class="col-md-6">
+            {{-- <div class="col-md-6">
                 <!--begin::Google link--->
                 <a href="{{ url('/auth/redirect/google') }}?redirect_uri={{ url()->current() }}" class="btn btn-flex btn-outline btn-text-gray-700 btn-active-color-primary bg-state-light flex-center text-nowrap w-100">
                     <img alt="Logo" src="{{ image('svg/brand-logos/google-icon.svg') }}" class="h-15px me-3"/>
@@ -41,14 +41,18 @@
                     Sign in with Apple
                 </a>
                 <!--end::Google link--->
-            </div>
+            </div> --}}
             <!--end::Col-->
+            {{-- Logo centrado --}}
+            <a href="{{ route('dashboard') }}" class="mb-12" style="display: flex; justify-content: center;">
+                <img alt="Logo" src="{{ image('auth/secoem.svg') }}" class="h-60px h-lg-75px"/>
+            </a>
         </div>
         <!--end::Login options-->
 
         <!--begin::Separator-->
         <div class="separator separator-content my-14">
-            <span class="w-125px text-gray-500 fw-semibold fs-7">Or with email</span>
+            <span class="w-125px text-gray-500 fw-semibold fs-7">Ingresa con tu usuario</span>
         </div>
         <!--end::Separator-->
 
@@ -73,7 +77,7 @@
 
             <!--begin::Link-->
             <a href="{{ route('password.request') }}" class="link-primary">
-                Forgot Password ?
+                Olvidaste tu contraseña ?
             </a>
             <!--end::Link-->
         </div>
@@ -82,19 +86,19 @@
         <!--begin::Submit button-->
         <div class="d-grid mb-10">
             <button type="submit" id="kt_sign_in_submit" class="btn btn-primary">
-                @include('partials/general/_button-indicator', ['label' => 'Sign In'])
+                @include('partials/general/_button-indicator', ['label' => 'Iniciar Sesión'])
             </button>
         </div>
         <!--end::Submit button-->
 
         <!--begin::Sign up-->
-        <div class="text-gray-500 text-center fw-semibold fs-6">
+        {{-- <div class="text-gray-500 text-center fw-semibold fs-6">
             Not a Member yet?
 
             <a href="{{ route('register') }}" class="link-primary">
                 Sign up
             </a>
-        </div>
+        </div> --}}
         <!--end::Sign up-->
     </form>
     <!--end::Form-->
