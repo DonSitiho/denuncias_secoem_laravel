cb04cbf453a5415f0e55529bca4ff2f28a12c3f2
Osiris Aguilazochom <osiris.sagi@gmail.com>
2025-10-31 13:51:24 -0600

    Se quitaron los menús que no se utilizan

--- resources/views/partials/menus/_user-account-menu.blade.php
+++ resources/views/partials/menus/_user-account-menu.blade.php
@@ -1,4 +1,5 @@
 <!--begin::User account menu-->
+{{-- Desactivar Menu de Usuario --}}
 <div class="menu menu-sub menu-sub-dropdown menu-column menu-rounded menu-gray-800 menu-state-bg menu-state-color fw-semibold py-4 fs-6 w-275px" data-kt-menu="true">
     <!--begin::Menu item-->
     <div class="menu-item px-3">
@@ -17,7 +18,7 @@
             <!--begin::Username-->
             <div class="d-flex flex-column">
                 <div class="fw-bold d-flex align-items-center fs-5">{{ Auth::user()->name}}
-                    <span class="badge badge-light-success fw-bold fs-8 px-2 py-1 ms-2">Pro</span>
+                    {{-- <span class="badge badge-light-success fw-bold fs-8 px-2 py-1 ms-2">Pro</span> --}}
                 </div>
                 <a href="#" class="fw-semibold text-muted text-hover-primary fs-7">{{ Auth::user()->email }}</a>
             </div>
@@ -30,21 +31,21 @@
     <!--end::Menu separator-->
     <!--begin::Menu item-->
     <div class="menu-item px-5">
-        <a href="#" class="menu-link px-5">My Profile</a>
+        <a href="#" class="menu-link px-5">Mi Perfil</a>
     </div>
     <!--end::Menu item-->
     <!--begin::Menu item-->
-    <div class="menu-item px-5">
+    {{-- <div class="menu-item px-5">
         <a href="#" class="menu-link px-5">
             <span class="menu-text">My Projects</span>
             <span class="menu-badge">
                 <span class="badge badge-light-danger badge-circle fw-bold fs-7">3</span>
             </span>
         </a>
-    </div>
+    </div> --}}
     <!--end::Menu item-->
     <!--begin::Menu item-->
-    <div class="menu-item px-5" data-kt-menu-trigger="{default: 'click', lg: 'hover'}" data-kt-menu-placement="left-start" data-kt-menu-offset="-15px, 0">
+    {{-- <div class="menu-item px-5" data-kt-menu-trigger="{default: 'click', lg: 'hover'}" data-kt-menu-placement="left-start" data-kt-menu-offset="-15px, 0">
         <a href="#" class="menu-link px-5">
             <span class="menu-title">My Subscription</span>
             <span class="menu-arrow"></span>
@@ -87,12 +88,12 @@
             <!--end::Menu item-->
         </div>
         <!--end::Menu sub-->
-    </div>
+    </div> --}}
     <!--end::Menu item-->
     <!--begin::Menu item-->
-    <div class="menu-item px-5">
+    {{-- <div class="menu-item px-5">
         <a href="#" class="menu-link px-5">My Statements</a>
-    </div>
+    </div> --}}
     <!--end::Menu item-->
     <!--begin::Menu separator-->
     <div class="separator my-2"></div>
@@ -100,14 +101,14 @@
     <!--begin::Menu item-->
     <div class="menu-item px-5" data-kt-menu-trigger="{default: 'click', lg: 'hover'}" data-kt-menu-placement="left-start" data-kt-menu-offset="-15px, 0">
         <a href="#" class="menu-link px-5">
-			<span class="menu-title position-relative">Mode 
+			<span class="menu-title position-relative">Modo 
 			<span class="ms-5 position-absolute translate-middle-y top-50 end-0">{!! getIcon('night-day', 'theme-light-show fs-2') !!} {!! getIcon('moon', 'theme-dark-show fs-2') !!}</span></span>
 		</a>
 		@include('partials/theme-mode/__menu')
 	</div>
 	<!--end::Menu item-->
 	<!--begin::Menu item-->
-	<div class="menu-item px-5" data-kt-menu-trigger="{default: 'click', lg: 'hover'}" data-kt-menu-placement="left-start" data-kt-menu-offset="-15px, 0">
+	{{-- <div class="menu-item px-5" data-kt-menu-trigger="{default: 'click', lg: 'hover'}" data-kt-menu-placement="left-start" data-kt-menu-offset="-15px, 0">
 		<a href="#" class="menu-link px-5">
             <span class="menu-title position-relative">Language
                 <span class="fs-8 rounded bg-light px-3 py-2 position-absolute translate-middle-y top-50 end-0">English
@@ -163,17 +164,17 @@
             <!--end::Menu item-->
         </div>
         <!--end::Menu sub-->
-    </div>
+    </div> --}}
     <!--end::Menu item-->
     <!--begin::Menu item-->
     <div class="menu-item px-5 my-1">
-        <a href="#" class="menu-link px-5">Account Settings</a>
+        <a href="#" class="menu-link px-5">Configuración</a>
     </div>
     <!--end::Menu item-->
     <!--begin::Menu item-->
     <div class="menu-item px-5">
         <a class="button-ajax menu-link px-5" href="#" data-action="{{ route('logout') }}" data-method="post" data-csrf="{{ csrf_token() }}" data-reload="true">
-            Sign Out
+            Cerrar Sesión
         </a>
     </div>
     <!--end::Menu item-->
