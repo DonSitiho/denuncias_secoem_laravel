973fe3bb19129c501956abc4320ce8dcbc5d4e89
Osiris Aguilazochom <osiris.sagi@gmail.com>
2026-08-13 11:32:44 -0600

    cambio de librerias para css en navbar de inicio

--- resources/views/denuncias/layout/navbar.blade.php
+++ resources/views/denuncias/layout/navbar.blade.php
@@ -1,8 +1,12 @@
-<link rel="stylesheet" href="https://michoacan.gob.mx/cdn/css/bootstrap.min.css">
+{{-- <link rel="stylesheet" href="https://michoacan.gob.mx/cdn/css/bootstrap.min.css">
         <link rel="stylesheet" href="https://michoacan.gob.mx/cdn/css/estilos.css">
         
         <link rel="shortcut icon" href="https://michoacan.gob.mx/cdn/img/favicon/favicon.ico" type="image/x-icon" />
-        <link rel="stylesheet" href="{{ asset('assets/css/estiloGob.css') }}">
+        <link rel="stylesheet" href="{{ asset('assets/css/estiloGob.css') }}"> --}}
+
+        <link rel="stylesheet" href="{{ asset('assets/vendor/bootstrap/bootstrap.min.css') }}">
+<link rel="stylesheet" href="{{ asset('assets/css/estilos.css') }}">
+<link rel="shortcut icon" href="{{ asset('assets/img/favicon.ico') }}" type="image/x-icon">
 
         <!-- Navbar Oficial -->
         <nav class="navbar main-nav fixed-top navbar-expand-lg">
